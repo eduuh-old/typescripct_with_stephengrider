813a6d825556299bcd4c1a72296f955ae22aebd8
Edwin Kamau Muraya <edwinkamaumuraya0@outlook.com>
2019-09-14 22:45:20 +0300

    finished with bubble sort algorithm

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,23 @@
-console.log('hi there');
-const logsomething = () => {
-  console.log('123')
-};
-logsomething();
+class Sorter{
+  constructor(public collection: number[]) {
+    this.collection = collection; 
+  }
+  //implemetation of Bubble sort algorithm
+  sort():void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++){
+      for (let j = 0; j < length - i - 1; j++){
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+}
+  
+}
+
+const sorter = new Sorter([10, 3, -5, 1]);
+sorter.sort();
+console.log(sorter.collection);
