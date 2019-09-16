05ef80da3680d1e0d6ab498979b5e6d88f36c8f7
Edwin Kamau Muraya <edwinkamaumuraya0@outlook.com>
2019-09-16 12:03:28 +0300

    Finished implementing the sorting of a linked list in my Project

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,8 @@
 import { Sorter } from "./sorter";
 import { NumbersCollections } from "./numberscollectins";
 import { CharacterCollection } from "./CharacterCollection";
-
+import { LinkedList } from './LinkedList';
+//
 const numbercollections = new NumbersCollections([20, 10, 3, -5, 0]);
 const sort = new Sorter(numbercollections);
 sort.sort();
@@ -11,3 +12,13 @@ const charactercollections = new CharacterCollection("Edwin kamau");
 const sorter = new Sorter(charactercollections);
 sorter.sort();
 console.log(charactercollections);
+
+const linkedlist = new LinkedList();
+linkedlist.add(500);
+linkedlist.add(-10);
+linkedlist.add(3);
+linkedlist.add(3);
+
+const linkedsorter = new Sorter(linkedlist);
+linkedsorter.sort();
+linkedlist.print();
